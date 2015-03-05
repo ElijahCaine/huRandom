c66b2cc6f6a02b16339292017e7e2efa8e7ebf75
Elijah Caine <elijahcainemv@gmail.com>
2015-03-04 23:04:07 -0800

    Alpha as fuck release Additions: - Footer   - CC ShareAlike 4.0 for Data   - Links to Flask, Pygal, Bootstrap, and GH page - Cleaned up UI drastically - Added shell about page - Added better admin page w/ features - Got graphs actually working

--- huRandom/hurandom.py
+++ huRandom/hurandom.py
@@ -8,7 +8,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 import csv
 
 import pygal
-
+from pygal.style import CleanStyle
 
 app = Flask(__name__)
 app.config.from_envvar('HURANDOM_SETTINGS', silent=True)
@@ -19,6 +19,7 @@ def connect_db():
     Connect to a given DB defined in conf.py and exported to env vars
     """
     return sqlite3.connect(app.config['DATABASE'])
+
 def init_db():
     """
     Initializes database. `$ python`, `>from flask import init_db`, `>init_db()`
@@ -49,7 +50,22 @@ def fetch_entries():
     Fetches entries from database and returns dict.
     """
     cur = g.db.execute('select text from entries order by id desc')
-    entries = [dict(text=row[0]) for row in cur.fetchall()]
+    #entries = list(float(row[0]) for row in cur.fetchall())
+    entries = []
+    for row in cur.fetchall():
+        try:
+            new = float(row[0])
+        except:
+            pass
+        try:
+            new = int(row[0])
+        except:
+            new = None
+        if new is not None:
+            entries.append(new)
+
+
+    entries.sort()
     return entries
 
 @app.route('/')
@@ -78,10 +94,12 @@ def goto_data():
     Links user to data page.
     """
     nums = fetch_entries()
-    return render_template('data.html', entries=nums)
+    chart = graph_data(fetch_entries())
+    print(nums)
+    return render_template('data.html', entries=nums, chart=chart)
 
 @app.route('/admin')
-def admin_interface():
+def admin_page():
     """
     Renders admin interface page.
     """
@@ -101,7 +119,7 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('admin_interface'))
+            return redirect(url_for('admin_page'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
@@ -113,27 +131,35 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('add_entry_page'))
 
+@app.route('/csv')
 def export_csv(data):
     """
     Exports data into newline delimited csv file
     """
-    csvfile = 'data/hurandom-raw.csv'
+    csvfile = 'data/hr_data.csv'
     with open(csvfile, "w") as output:
         writer = csv.writer(output, lineterminator='\n')
         for val in data:
             writer.writerow([val]) 
+    return redirect(url_for('goto_data'))
 
 def graph_data(data):
     """
     Generates histogram of data for user
     """
-    chart = pygal.Bar()
+    chart = pygal.Bar(fill=True, interpolate='cubic', style=CleanStyle, no_data_text='Not enough data to science :(', title_font_size=40, no_data_font_size=30)
     chart.title = '`Random` Numbers'
-    #chart.x_labels = map(str, range(2002, 2013))
+    #chart.x_labels = map(str, range(min(data), max(data)))
     chart.add('Data', data)
     chart.render()
-    chart.render_to_file('data/chart.svg')  
-    
+    chart = chart.render(is_unicode=True)
+    return chart
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+init_db()
 
 if __name__ == '__main__':
     app.run()
